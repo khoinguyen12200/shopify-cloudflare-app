8852e264bdee61ffb1df1c4b97d69e607a6988f3
Nguyen Khoi <nguyenkhoi@Mac-Air.local>
2026-09-08 11:44:59 +0700

    fix(migrations): add subscription revision only once

--- drizzle/0018_brave_tyrannus.sql
+++ drizzle/0018_brave_tyrannus.sql
@@ -1,3 +1,2 @@
 ALTER TABLE `shop_subscriptions` ADD `cancel_effective_on` text;--> statement-breakpoint
 ALTER TABLE `shopify_subscription_events` ADD `cancel_effective_on` text;
-ALTER TABLE `shop_subscriptions` ADD `revision` integer NOT NULL DEFAULT 1;
